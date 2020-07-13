4c95661d3f39f76369f628f81e928ecc1cd0d2de
unknown <brw1820@gmail.com>
2020-07-13 12:37:04 -0400

    Created Heroku connection and JawsDB

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ghetto Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whattaburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Five Guys', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shaceshack', true);
--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-const enquirer = requirer('enquirer');
+const exphbs = require("express-handlebars");
+const express = require("express");
+const mysql = require("mysql");
+const orm = require('orm');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "milkyway",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+app.get("/", function (req,res) {
+  response.send("hello world!")
+})
