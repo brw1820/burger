0f69ffa24fb23b7189ab2adb7eb17ec5f0f13024
unknown <brw1820@gmail.com>
2020-07-14 16:21:43 -0400

    built orm and burger_controller files

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,4 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Ghetto Burger', TRUE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Whattaburger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Five Guys', TRUE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Shaceshack', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shakeshack', true);
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require("express");
+const express = require("express");
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -10,21 +10,17 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/catsController.js");
-const { response } = require("express");
+const routes = require("./controllers/burgers_controller.js");
 
+app.use(routes);
 
-app.get("/", function (req,res) {
-  response.send("hello world!")
-})
 
 app.listen(PORT, function() {
-
   console.log("Server listening on: http://localhost:" + PORT);
 });
 
