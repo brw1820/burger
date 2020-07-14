dbc12bd759460fe2eca6ad7717465ddd429f5732
unknown <brw1820@gmail.com>
2020-07-14 19:39:34 -0400

    server.js changes

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 const exphbs = require("express-handlebars");
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
-=======
-var express = require("express");
->>>>>>> ce60edebd20c8812f476ff32eba1658795eaec51
 
 var PORT = process.env.PORT || 8080;
 
@@ -22,19 +18,10 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-<<<<<<< HEAD
-var routes = require("./controllers/burgers_controller.js");
+const routes = require("./controllers/burgers_controller.js");
+
 app.use(routes);
-
-// start the server listening
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
-});
-=======
-var routes = require("./controllers/catsController.js");
-const { response } = require("express");
-
->>>>>>> ce60edebd20c8812f476ff32eba1658795eaec51
+// const { response } = require("express");
 
 app.get("/", function (req,res) {
   response.send("hello world!")
